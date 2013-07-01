f6ea3708805a791a0fe4dd675fcd5abf71a364a4
Ikenna Nwaiwu <ikenna.nwaiwu@itv.com>
2013-07-01 09:15:58 +0100

    update

---
+++ scalaimports/src/packageobjects.scala
@@ -0,0 +1,22 @@
+package net.ikenna.toys
+
+package object trains {
+  /* Package objects are a good place to put utility functions and fields that apply to the package*/
+  def  move = println("choo choo choo")
+
+  /*On a real project, this package object would be in file net/ikenna/toys/trains/package.scala */
+}
+
+
+package trains {
+  class Train4 {
+    def moveTrain = move
+  }
+
+  object Test{
+    def main(args: Array[String]) {
+      new Train4().moveTrain
+    }
+  }
+}
+
---
+++ scalaimports/src/packagevisibility.scala
@@ -0,0 +1,23 @@
+package net.ikenna.toys.trains{
+
+  class Train5{
+    private[trains] def move = println("choo choo choo")
+    //Scope of protection: private[trains] means move is private to class Train 5 but also visible in package trains.
+    // Note that it will not be visible from package toys.
+  }
+
+  object TestTrain5{
+    def main(args: Array[String]) {
+      new Train5().move  /*We are only able to access private method move, because of our scope of protection rule*/
+    }
+  }
+}
+
+
+package net.ikenna.toys{
+  object TestTrain5Again{
+    def main(args: Array[String]) {
+//      new trains.Train5().move()   // wouldnt compile. Shows Train5.move is not visible from toys
+    }
+  }
+}
